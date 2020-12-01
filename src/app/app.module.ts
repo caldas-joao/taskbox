cda07f2dc9f669d8bcc2b634a5a6218988add55c
caldas-joao <joao.caldas@mhr.co.uk>
2020-12-01 10:27:43 +0000

    taskbox UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { RouterModule, Routes } from  '@angular/router';
+
+const  appRoutes:  Routes  = [
+  { path:  'customer-list', component:  CustomerListComponent },
+  { path:  'customer-create', component:  CustomerCreateComponent },
+  {
+  path:  'login',
+  component:  LoginComponent
+  },
+  { path:  '',
+  
+  redirectTo:  '/login',
+  
+  pathMatch:  'full'
+  
+  },
+  ];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CustomerListComponent,
+    CustomerCreateComponent
   ],
   imports: [
     BrowserModule,
